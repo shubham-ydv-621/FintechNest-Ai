d5a4dcc21cd2004a9298f443a12e1a31be794024
shubham-ydv-621 <shubham2006621@gmail.com>
2025-07-06 14:17:03 +0530

    added hero section,banner theme properties and useeffect and applies cool themes in global css

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
-import { Button } from "@/components/ui/button";
+import HeroSection from "@/components/hero";
+
 
 export default function Home() {
   return (
-   <Button variant="destructive">helllu</Button>
+    <div className="mt-40">
+   <HeroSection />
+   </div>
   );
 }
