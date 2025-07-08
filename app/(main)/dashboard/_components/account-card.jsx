51611c07af085494e25fe4f8f87a10b8a2414dfc
shubham-ydv-621 <shubham2006621@gmail.com>
2025-07-09 00:59:46 +0530

    implemented for auto turn of feture in default account when turn one off other goes off next on to account page on clicking account

--- app/(main)/dashboard/_components/account-card.jsx
+++ app/(main)/dashboard/_components/account-card.jsx
@@ -1,12 +1,51 @@
+"use client"
+
 import React from 'react'
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from '@/components/ui/card'
 import { Switch } from '@/components/ui/switch'
 import { ArrowUpRight, ArrowDownRight } from 'lucide-react'
 import Link from 'next/link'
+import  useFetch  from '@/hooks/use-fetch'
+import { updateDefaultAccount } from '@/actions/accounts'
+import { toast } from 'sonner'
+import { useEffect } from 'react'
+  
+
 
 const AccountCard=({ account })=>{
   const { name, type, balance, id, isDefault } = account;
  
+  
+  const { 
+    loading: updateDefaultLoading,
+    fn: updateDefaultFn,
+    data: updatedAccount,
+    error,
+  } = useFetch(updateDefaultAccount);
+   
+   const handleDefaultChange = async (event) => {
+    event.preventDefault(); // Prevent navigation
+
+    if (isDefault) {
+      toast.warning("You need atleast 1 default account");
+      return; // Don't allow toggling off the default account
+    }
+
+    await updateDefaultFn(id);
+  };
+
+    useEffect(() => {
+    if (updatedAccount?.success) {
+      toast.success("Default account updated successfully");
+    }
+  }, [updatedAccount,updateDefaultLoading]);
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error.message || "Failed to update default account");
+    }
+  }, [error]); 
+
   return (
      <Card className="hover:shadow-md transition-shadow group relative">
       <Link href={`/account/${id}`}>
@@ -16,8 +55,9 @@ const AccountCard=({ account })=>{
           </CardTitle>
           <Switch
             checked={isDefault}
-         
-          /> 
+            onClick={handleDefaultChange}
+            disabled={updateDefaultLoading}
+          />
         </CardHeader>
         <CardContent>
           <div className="text-2xl font-bold">
