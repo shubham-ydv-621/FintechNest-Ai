91fe904c1ed22fec43626d6fd52961adbbd9ceec
shubham-ydv-621 <shubham2006621@gmail.com>
2025-07-12 17:24:47 +0530

    implemented monthly budget progress bar using progress ui component and budget-progress and page.jsx in dashboard nest is to alert using ingest

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -4,14 +4,28 @@ import { Card, CardContent } from '@/components/ui/card';
 import { Plus } from 'lucide-react';  
 import { getUserAccounts } from '@/actions/dashboard';
 import AccountCard from './_components/account-card';
+import { getCurrentBudget } from "@/actions/budget";
+import { BudgetProgress } from "./_components/budget-progress";
+
 
 async function DashboardPage()  {
 const accounts=await getUserAccounts();
 
+  const defaultAccount = accounts?.find((account) => account.isDefault);
+
+  // Get budget for default account
+  let budgetData = null;
+  if (defaultAccount) {
+    budgetData = await getCurrentBudget(defaultAccount.id);
+  }
 
   return (
-    <div className='px-5'>
+    <div className='space-y-8'>
     {/*budget progress*/}
+     <BudgetProgress
+        initialBudget={budgetData?.budget}
+        currentExpenses={budgetData?.currentExpenses || 0}
+      /> 
 
      {/*overview*/}
 
